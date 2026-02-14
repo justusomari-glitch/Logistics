fe89f455da7464fa7519cb5a885d227266ac90eb
Omari Kwache <justusomari@gmail.com>
2026-02-14 14:29:04 +0300

    Upgraded App

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,17 @@ with st.form(key="customer_form"):
     form_values2['Revenue generated']=st.number_input("Enter Revenue Generated:")
     form_values2["Availability"]=st.number_input("Enter Availability:",min_value=3)
     submit_button=st.form_submit_button(label="Customer Segmentation:")
-    if None in form_values2.values():
-        st.warning("Please fill in all the fields.")
-    else:input_data={
-        'Customer demographics': form_values2['Customer demographics'],
-        'Product type': form_values2['Product type'],
-        'Price': form_values2['Price'],
-        'Number of products sold': form_values2['Number of products sold'],
-        'Revenue generated': form_values2['Revenue generated'],
-        "Availability": form_values2["Availability"]
-    }
-url="https://logistics-2-ever.onrender.com/predict/customer_segmentation"
+    if submit_button:
+        input_data={
+            'Customer_demographics': form_values2['Customer demographics'],
+            'Product_type': form_values2['Product type'],
+            'Price': form_values2['Price'],
+            'Number_of_products_sold': form_values2['Number of products sold'],
+            'Revenue_generated': form_values2['Revenue generated'],
+            'Availability': form_values2["Availability"] 
+        }
+        url="https://logistics-2-ever.onrender.com/predict"
+
 
 response=requests.post(url,json=input_data)
 st.write(response)
